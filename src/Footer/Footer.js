76575fa8dd8a5b43b8226f3c8af613e4cc23c0ac
InhuJo <inhudev@gmail.com>
2021-08-26 01:57:56 +0900

    Responsive footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,20 +1,39 @@
 import './Footer.css';
 import React from 'react';
+import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa";
 
 function Footer() {
     return (
-        <div style={{height: '200px'}}>
-            <div>
-                ECOSTEP
-            </div>
-            <div>
-                대구광역시 북구 대학로 80, 경북대학교
-            </div>
-            <div>
-                대표번호 010-1234-5678
-            </div>
-            <div>
-                © ECOFRIENDS. ALL RIGHTS RESERVED. 
+        <div className="footer">
+            <div className="footer-inner">
+                <div className="footer-left">
+                    <div className="footer-logo">
+                        ECOSTEP
+                    </div>
+                    <div>
+                        대구광역시 북구 대학로 80, 경북대학교
+                    </div>
+                    <div>
+                        대표번호 010-1234-5678
+                    </div>
+                    <div style={{ marginTop: '5px' }}>
+                        © ECOFRIENDS. ALL RIGHTS RESERVED.
+                    </div>
+                </div>
+                <div className="footer-right">
+                    <div className="footer-icons">
+                        <div>
+                            <FaFacebookSquare />
+                        </div>
+                        <div>
+                            <FaInstagramSquare />
+                        </div>
+                        <div>
+                            <FaBootstrap />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import './Navbar.css';
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FiMenu, FiX } from 'react-icons/fi';
 
 function Navbar() {
@@ -10,36 +10,38 @@ function Navbar() {
     }
 
     return (
-       <nav className="navbar">
-           <Link to="/" className="nav-logo" onClick={onclick}>
-               ECOSTEP
-           </Link>
-           <ul className={isToggled ? 'nav-links active':'nav-links'}>
-               <li className="nav-item">
-                   <Link to="/" className="nav-link" onClick={onclick}>
-                       어플 소개
-                   </Link>
-               </li>
-               <li className="nav-item">
-                   <Link to="/" className="nav-link" onClick={onclick}>
-                       현황
-                   </Link>
-               </li>
-               <li className="nav-item">
-                   <Link to="/" className="nav-link" onClick={onclick}>
-                       관련 행사
-                   </Link>
-               </li>
-               <li className="nav-item">
-                   <Link to="/" className="nav-link" onClick={onclick}>
-                       문의
-                   </Link>
-               </li>
-           </ul>
-           <div onClick={() => setisToggled(!isToggled)} className="nav-icon">
-               {isToggled ? <FiX/> : <FiMenu/>}
-           </div>
-       </nav>
+        <nav className="navbar">
+            <div className="navbar-inner">
+                <Link to="/" className="nav-logo" onClick={onclick}>
+                    ECOSTEP
+                </Link>
+                <ul className={isToggled ? 'nav-links active' : 'nav-links'}>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" onClick={onclick}>
+                            어플 소개
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" onClick={onclick}>
+                            현황
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" onClick={onclick}>
+                            관련 행사
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" onClick={onclick}>
+                            문의
+                        </Link>
+                    </li>
+                </ul>
+                <div onClick={() => setisToggled(!isToggled)} className="nav-icon">
+                    {isToggled ? <FiX /> : <FiMenu />}
+                </div>
+            </div>
+        </nav>
     )
 }
 
