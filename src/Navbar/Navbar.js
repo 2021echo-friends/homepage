5ee4f4c0ad6617b35f01cd24fa037efbfc16bcc7
InhuJo <inhudev@gmail.com>
2021-08-26 16:39:09 +0900

    Fix navbar scroll eventlistener - use style.removeProperty() instead of style property edit

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -16,11 +16,10 @@ function Navbar() {
         if(afterPosition >= 80) {   
             navBar.style.background="white";
             navBar.style.boxShadow="rgba(149, 157, 165, 0.4) 0px 8px 24px"
-            // box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
         }
         else if (afterPosition < 80) {
-            document.getElementById('navbar').style.background="none";
-            navBar.style.boxShadow="none";
+            navBar.style.removeProperty('background');
+            navBar.style.removeProperty('box-shadow');
         }
     })
 
