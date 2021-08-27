223823f7ffe5682bd41cd1dd02bf5810bf8d11db
InhuJo <inhudev@gmail.com>
2021-08-28 00:10:41 +0900

    Update MainPoint page

--- src/MainPage/Components/MainHome.js
+++ src/MainPage/Components/MainHome.js
@@ -5,25 +5,25 @@ import CountUp from 'react-countup';
 function MainHome() {
     return (
         <div className="home-inner">
-            <div className="inner-top">
-                <div className="top-left">
-                    <div id="title-area">
+            <div className="home-top">
+                <div className="home-top-left">
+                    <div id="home-title-area">
                         <img
                             src={process.env.PUBLIC_URL + '/images/plant.png'}
                             alt='plant img'
                             width='60px'
                             height='50px'
                         />
-                        <div id="inner-title">
+                        <div id="home-inner-title">
                             지구를 지키는 한 걸음
                         </div>
                     </div>
-                    <div id="title-desc">
+                    <div id="home-title-desc">
                         <div>일상에서 시작하는 친환경 소비 프로젝트</div>
                         <div>지구를 지키는 일이 바로 우리를 지키는 길입니다</div>
                     </div>
                 </div>
-                <div className="top-right">
+                <div className="home-top-right">
                     <div>
                         <span>
                             <CountUp
@@ -43,20 +43,15 @@ function MainHome() {
                     </button> */}
                 </div>
             </div>
-            <div className="inner-bottom">
-                <div className="bottom-left">
-                    <img
-                        src={process.env.PUBLIC_URL + '/images/main_trees.png'}
-                        alt="tree img"
-                        width="1070px"
-                        height="350px"
-                    />
-                </div>
-                <div className="bottom-right">
-                    <div>
-
-                    </div>
-                </div>
+            <div className="home-bottom">
+                <img
+                    src={process.env.PUBLIC_URL + '/images/main_trees.png'}
+                    alt="tree img"
+                    width="1070px"
+                    height="350px"
+                />
+                {/* <div className="bottom-left"></div>
+                <div className="bottom-right"></div> */}
             </div>
         </div>
     )
--- src/MainPage/Components/MainPoint.js
+++ src/MainPage/Components/MainPoint.js
@@ -1,9 +1,38 @@
+import '../MainPage.css';
 import React from 'react';
 
 function MainPoint() {
     return (
         <div className="point-inner">
-            
+            <div className="point-top">
+                <div className="point-top-title">
+                    <div>가치있는 소비,</div>
+                    <div>
+                        <span>ECOSTEP</span>
+                        에서 시작하세요
+                    </div>
+                </div>
+            </div>
+            <div className="point-bottom">
+                <div className="point-bottom-img">
+                    <img 
+                        src={process.env.PUBLIC_URL + '/images/main_products.png'} 
+                        alt='main product img'
+                        width='500px'
+                        style={{paddingTop:'30px'}}
+                    />
+                </div>
+                <div className="point-bottom-desc">
+                    <div>
+                        에코스텝 제품을 구입하고 <br/> 
+                        에코포인트를 적립할 수 있습니다
+                    </div>
+                    <div>
+                        포인트를 모아 다시 에코스텝 제품을 <br/> 
+                        구입할 수 있어요
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
