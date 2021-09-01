a3df75ceb9ff8fedcdbb5d6a4f45a761ce9b363e
InhuJo <inhudev@gmail.com>
2021-09-01 22:43:18 +0900

    Fix scroll not moving to the top when moving pages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import ScrollTop from './ScrollTop';
 
 ReactDOM.render(
   <Router>
+    <ScrollTop/>
     <App/>
   </Router>,
   document.getElementById('root')
